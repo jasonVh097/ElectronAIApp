419cfc8dd57fffe60bbebcc37298122316eb6136
Jason <jason.van.hootegem@student.howest.be>
2025-03-18 11:12:03 +0100

    remove console log

---
+++ services/fileService.js
@@ -0,0 +1,21 @@
+const filesToZip = [];
+
+const addFileButton = document.querySelector('#addFile');
+const fileInput = document.querySelector('#pdfFile');
+const fileList = document.querySelector('ul#pdfFiles');
+
+addFileButton.addEventListener('click', () => {
+    addFile();
+})
+
+function addFile(){
+    const file = fileInput.files[0];
+    if (file) {
+        console.log("there is a file");
+        filesToZip.push(file);
+        const li = document.createElement('li');
+        li.textContent = file.name;
+        fileList.appendChild(li);
+    }
+}
+
